645585003a14963efc2a179dee57a6d88be18cbd
steff1461 <steffen.w@outlook.be>
2021-04-17 21:23:34 +0200

    My first API Platform app running on Heroku!

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
-use Symfony\Component\Serializer\Annotation\groups;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Serializer\Annotation\SerializedName;
 use Symfony\Component\Security\Core\User\UserInterface;
 
